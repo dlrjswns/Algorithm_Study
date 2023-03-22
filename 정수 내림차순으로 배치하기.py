764e75c6e13b6561b7a8fb3a587b44a032d394df
dlrjswns <leekyunjun@gmail.com>
2023-03-22 22:13:02 +0900

    [ADD] 정수 내림차순으로 배치하기 주석

--- 정수 내림차순으로 배치하기.py
+++ 정수 내림차순으로 배치하기.py
@@ -4,3 +4,6 @@ def solution(n):
         
     return int("".join(ls))
 
+# list(map(int, str(n)))대신에 list(str(n))을 하는 이유는
+# 전자는 정수리스트가 후자는 문자리스트가 리턴되게 되는데 맨 마지막 join함수를 사용하기위해선
+# 초기 구분자를 ""로 했기때문에 ls가 문자리스트여야만 원하는 값을 얻을 수 있
