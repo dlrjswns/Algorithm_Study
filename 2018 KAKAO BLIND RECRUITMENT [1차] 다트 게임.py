041997be824b2e9bd038322e85d2da07678ac34b
dlrjswns <leekyunjun@gmail.com>
2023-03-23 17:20:33 +0900

    [ADD] 주석 추가

--- 2018 KAKAO BLIND RECRUITMENT [1차] 다트 게임.py
+++ 2018 KAKAO BLIND RECRUITMENT [1차] 다트 게임.py
@@ -25,3 +25,7 @@ def solution(dartResult):
             score[-1] = score[-1] * (-1)
         
     return sum(score)
+
+# isnumeric()은 해당 문자열에 숫자에 관련된게 존재할 경우 True 반환
+# 리스트에 대한 요소의 길이를 구할때 len(리스트)를 통해서 구할 수 있음
+# 리스트에 대한 인덱스로 접근할때 -1과 같은 음수를 통해서 맨 마지막 요소에 접근 가
